f1608e405550ccffba92f2d384aba040bcea497f
pinoke98 <40706319+pinoke98@users.noreply.github.com>
2020-10-07 09:12:45 -0500

    Se agregaron las fotos

--- CDataSet.py
+++ CDataSet.py
@@ -3,6 +3,7 @@ import numpy as np
 import xlsxwriter
 from glob import glob
 import matplotlib.pyplot as plt
+#from sklearn.preprocessing import StandardScaler
 
 i=1
 
@@ -11,7 +12,6 @@ col=0
 
 xlabel = []
 ylabel = []
-color = ["ro","bo"]
 
 #Nuevo libro excel
 databook = xlsxwriter.Workbook('DatosTrain.xlsx')
@@ -27,10 +27,9 @@ def SkinColorLower (Hue,mult1,mult2):
     lower = np.array(lower)
     return lower
 
-for j in range(1,3):
+for j in range(1,27):
     img_Carpeta = ('Fotos/'+str(j)+'/'+str(j)+' (*.jpg')
     img_names = glob(img_Carpeta)
-    #print(img_names)
     for fn in img_names:
         print(fn)
         img = cv2.imread(fn,1)
@@ -48,27 +47,32 @@ for j in range(1,3):
         mask = cv2.inRange(hls,SkinColorLower(0,0.2,0),SkinColorUpper(27,0.72,0.75))
         blur = cv2.medianBlur(mask,5)
         
+        # cv2.imshow("blur",blur)
+        # cv2.waitKey(0)
+        
         ret,edges = cv2.threshold(blur,0,255,cv2.THRESH_BINARY + cv2.THRESH_OTSU)
         kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(3,3))
         edges= cv2.morphologyEx(edges, cv2.MORPH_OPEN, kernel)
         
+        # cv2.imshow("ThreshHold", edges)
+        # cv2.waitKey(0)
+        
         contours, hierarchy = cv2.findContours(edges.copy(),cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_NONE)
         draw = cv2.drawContours(img,contours,-1,(255,0,0),3)
         
-        cv2.imshow("Imagen",img)
-        cv2.waitKey(0)
+        # cv2.imshow("Imagen",img)
+        # cv2.waitKey(0)
         
         c = max(contours, key = cv2.contourArea)
         x,y,w,h = cv2.boundingRect(c)
-        p = cv2.arcLength(c,True)
         empty = np.zeros((h,w),np.uint8)
         cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),1)
         roi = edges[y:y+h,x:x+w]
         empty[0:h,0:w]=roi
         edges=empty
         
-        cv2.imshow("Edges",edges)
-        cv2.waitKey(0)
+        # cv2.imshow("Edges",edges)
+        # cv2.waitKey(0)
         
         img_resize = cv2.resize(edges,(100,100),interpolation=cv2.INTER_AREA)
         ret, edges_res = cv2.threshold(img_resize,0,255,cv2.THRESH_BINARY + cv2.THRESH_OTSU)
@@ -81,34 +85,31 @@ for j in range(1,3):
         # cv2.imshow("img_resize",edges_res)
         # cv2.waitKey(0)
         
-        for component_2 in zip(contours_2,hierarchy_2):
-            currentContour =  component_2[0]
-            x,y,w,h = cv2.boundingRect(currentContour)
-            M = cv2.moments(currentContour)
-            cx = M['m10']/M['m00']
-            cy = M['m01']/M['m00']
-            A = cv2.contourArea(currentContour)
-            p = cv2.arcLength(currentContour,True)
-            aP=A/float(p*p)
-            Hu = cv2.HuMoments(M)
-            aT=np.count_nonzero(edges_res)
-            Comp = aT/float(p*p)
-            hull = cv2.convexHull(currentContour)
+        currentContour = max(contours_2, key = cv2.contourArea)
+        x,y,w,h = cv2.boundingRect(c)
+        M = cv2.moments(currentContour)
+        cx = M['m10']/M['m00']
+        cy = M['m01']/M['m00']
+        A = cv2.contourArea(currentContour)
+        p = cv2.arcLength(currentContour,True)
+        aP=A/float(p*p)
+        Hu = cv2.HuMoments(M)
+        aT=np.count_nonzero(edges_res)
+        Comp = aT/float(p*p)
+        hull = cv2.convexHull(currentContour)
+        
+        VectorCarac = np.array([Hu[0],Hu[1],Hu[2],Hu[3],Hu[4],Hu[5],Hu[6],M['m00'],M['m01'],M['m10'],M['m11'],aT,Comp])
+        for carac in (VectorCarac):
+                worksheet.write(row,col,str(j))
+                worksheet.write(row,i, carac)
+                i=i+1
+        i=1
+        row+=1
             
-            VectorCarac = np.array([cx,cy,A,p,aP,Hu[0],Hu[1],Hu[2],Hu[3],Hu[4],Hu[5],Hu[6],M['m00'],M['m01'],M['m10'],M['m11'],aT,Comp])
-            for carac in (VectorCarac):
-                    worksheet.write(row,col,str(j))
-                    worksheet.write(row,i, carac)
-                    i=i+1
-            i=1
-            row+=1
-            
-            print(Hu[4])
-            xlabel.append(j)
-            ylabel.append(Hu[4])
-            
-        # cv2.imshow("Imagen",img)
-        # cv2.waitKey(0)
+        print(Comp)
+        xlabel.append(j)
+        ylabel.append(Comp)
+
             
 plt.plot(xlabel,ylabel,"bo")
 plt.show()
--- Pruebas.py
+++ Pruebas.py
@@ -4,6 +4,7 @@ Created on Tue Sep  8 17:32:15 2020
 
 @author: jorge
 """
+"""
 import matplotlib.pyplot as plt
 
 x = [1,2,3,4]
@@ -13,3 +14,66 @@ for i in range(len(x)):
     plt.figure()
     plt.plot(x[i],y[i],"ro")
     plt.show()
+"""
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import GridSearchCV
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import classification_report
+from sklearn.neural_network import MLPClassifier
+import pandas as pd
+
+data  = pd.read_excel("DatosTrain.xlsx")
+data = np.array(data).astype(np.float64)
+data = data.transpose()
+y = data[0]
+x = data[1:18].transpose()
+
+x = StandardScaler().fit_transform(x)
+
+# Split the dataset in two equal parts
+X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=0)
+
+# Set the parameters by cross-validation
+parameters={
+'learning_rate': ["constant", "invscaling", "adaptive"],
+'hidden_layer_sizes': [(10,), (20,), (30,)],
+'alpha': [0.000001],
+'activation': ["logistic", "relu", "Tanh"]
+}
+
+scores = ['precision', 'recall']
+
+print(type(X_train))
+
+mlp = MLPClassifier(max_iter=100)
+
+for score in scores:
+    print("# Tuning hyper-parameters for %s" % score)
+    print()
+
+    clf = GridSearchCV(mlp,parameters,n_jobs=-1,cv=2,scoring='%s_macro'%score)
+    print("D")
+    clf.fit(X_train, y_train)
+
+    print("Best parameters set found on development set:")
+    print()
+    print(clf.best_params_)
+    print()
+    print("Grid scores on development set:")
+    print()
+    means = clf.cv_results_['mean_test_score']
+    stds = clf.cv_results_['std_test_score']
+    for mean, std, params in zip(means, stds, clf.cv_results_['params']):
+        print("%0.3f (+/-%0.03f) for %r"
+              % (mean, std * 2, params))
+    print()
+
+    print("Detailed classification report:")
+    print()
+    print("The model is trained on the full development set.")
+    print("The scores are computed on the full evaluation set.")
+    print()
+    y_true, y_pred = y_test, clf.predict(X_test)
+    print(classification_report(y_true, y_pred))
+    print()
